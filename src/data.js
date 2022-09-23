be15c580861000132acd96ba9196253c3af1d305
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-23 14:58:33 +0500

    add dogs data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const dogs = [
+  {
+    name: 'Rex',
+    avatar: 'images/dog-rex.jpg',
+    age: 25,
+    bio: 'Art. Literature. Natural wine. Yoga',
+    hasBeenSwiped: false,
+    haseBeenLiked: false
+  },
+  {
+    name: 'Bella',
+    avatar: 'images/dog-bella.jpg',
+    age: 43,
+    bio: "Yup, that's my owner. You can meet him if you want",
+    hasBeenSwiped: false,
+    haseBeenLiked: false
+  },
+  {
+    name: 'Teddy',
+    avatar: 'images/dog-teddy.jpg',
+    age: 30,
+    bio: "How you doing?",
+    hasBeenSwiped: false,
+    haseBeenLiked: false
+  }
+]
+
+export default dogs
